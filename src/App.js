f96aad877cda79ef2543e1f65f0f56900a1ff263
rohitkrsoni <krrohit37@gmail.com>
2023-04-30 22:03:52 +0530

    8. Implemented Routing

--- src/App.js
+++ src/App.js
@@ -5,14 +5,23 @@ import GlobalStyle from "./components/GlobalStyle";
 
 // Import Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+
+//Router
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/work" element={<OurWork />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
