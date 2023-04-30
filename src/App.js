a414b606b34c645afe9608479339c4d078f1fdd9
rohitkrsoni <krrohit37@gmail.com>
2023-05-01 00:38:26 +0530

    10. Add movies details page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
+import MovieDetail from "./pages/MovieDetail";
+
+// Import Components
 import Nav from "./components/Nav";
 
 //Router
@@ -21,6 +24,7 @@ function App() {
         <Route path="/" element={<AboutUs />} />
         <Route path="/work" element={<OurWork />} />
         <Route path="/contact" element={<ContactUs />} />
+        <Route path="/work/:id" element={<MovieDetail />} />
       </Routes>
     </div>
   );
